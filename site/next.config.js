37409072624a34a8a460b68d5001095d25c6c4a3
mikelee2082 <32311868+mikelee2082@users.noreply.github.com>
2023-11-28 20:56:34 +1300

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Rhetorica Ltd',
+    description: 'The official store for Griffin workspace images.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://rhetorica-software.github.io/griffin-kasm-registry/',
+    contactUrl: 'https://github.com/rhetorica-software/griffin-kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/griffin-kasm-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
